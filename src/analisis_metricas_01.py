77d95eae2346acefaf7bb62ca752f8ef7ded492c
edveralli <edveralli@gmail.com>
2024-06-13 11:19:43 -0300

    initial commit

---
+++ src/analisis_metricas_01.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  6 15:01:46 2024
+
+@author: 20171078343
+"""
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Cambio al directorio de datos
+os.chdir("../data/")
+
+# Cargar los datos desde el archivo CSV
+archivo_csv = "medidas_01.csv"
+df = pd.read_csv(archivo_csv)
+
+# Agrupar los datos por "hora" y calcular estadísticas descriptivas para "impacto"
+df_grouped = df.groupby('hora')['impacto'].describe()
+
+# Redondear las columnas 'mean' y 'std' a 2 decimales
+df_grouped['mean'] = df_grouped['mean'].round(2)
+df_grouped['std'] = df_grouped['std'].round(2)
+
+# Eliminar las columnas 25 y 75 del DataFrame agrupado
+df_grouped.drop(['25%', '75%'], axis=1, inplace=True)
+
+# Renombrar la columna '50%' como 'Mediana'
+df_grouped.rename(columns={'50%': 'Mediana'}, inplace=True)
+
+# Mostrar el DataFrame con las estadísticas descriptivas
+print(df_grouped)
+
+# Guardar los descriptores estadísticos en un archivo CSV
+descriptores_csv = "descriptores_estadisticos.csv"
+df_grouped.to_csv('../'+descriptores_csv, sep=';', encoding='utf-8-sig')
+
+# Graficar la distribución de "impacto" por "hora"
+plt.figure(figsize=(10, 6))
+plt.plot(df_grouped.index, df_grouped['mean'], marker='o', linestyle='-', color='b', label='Media')
+plt.fill_between(df_grouped.index, df_grouped['mean'] - df_grouped['std'], df_grouped['mean'] + df_grouped['std'], color='b', alpha=0.2, label='Desviación Estándar')
+plt.xlabel('Hora')
+plt.ylabel('Impacto')
+plt.title('Distribución de Impacto por Hora')
+plt.legend()
+plt.grid(True)
+plt.show()
